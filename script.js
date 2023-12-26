7c91659da4a5cb894c1639f2dac452d120683a2f
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-26 16:49:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        let a = 0;
+        let b = 1;
+        for (let i = 2; i <= num; i++) {
+            let temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 }
+
+// Examples
+// console.log(fibonacci(1)); // Output: 0
+// console.log(fibonacci(5)); // Output: 3
+
 
 module.exports = fibonacci;
